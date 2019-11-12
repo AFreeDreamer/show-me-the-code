230ea57c8f01ff2c4d513a3c1b8c23d20b71d6d4
zhengl <zhengl@succez.com>
2019-11-12 11:39:20 +0800

    0004

---
+++ answer/0004/T0004.py
@@ -0,0 +1,15 @@
+import re
+from collections import Counter
+
+
+
+def getWordsList(filename):
+    ret = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        [ret.extend(re.findall(r'[a-zA-Z]+', line)) for line in lines]
+    return ret
+
+print(Counter(getWordsList(r'.\words.txt')).most_common(10))
+
+# most_common(n) 返回出现频次最高的n个元素及其次数
