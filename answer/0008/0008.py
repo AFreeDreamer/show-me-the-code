175cc939f7fbda07a88b63a9fb1b3a1edfebde48
zhengl <zhengl@succez.com>
2019-11-19 15:01:38 +0800

    0008

--- answer/0008/0008.py
+++ answer/0008/0008.py
@@ -1,2 +1,20 @@
+# -*- coding: utf-8 -*-
 # 一个HTML文件，找出里面的**正文**。
 
+from bs4 import BeautifulSoup
+
+html = """
+<html> <head >< title > The  Dormouse ’ s  story</title></head>
+<body >
+<p  class =" title"  name="dromouse ”>< b> The  Dormouse  ’ s story</b></p>
+<p  class =” story ’'> Once  upon  a time  there  were  three  little  s isters;  and  their  names  were
+<a  href =” http://example.com/elsie " class= "  sister " id =” linkl ”><  ! - Elsie … >< la>,
+<a  href =” http://example.com/lacie " class =” sister " id =” link2 ”> Lacie</a>  and
+<a  href="http://example.com/tillie "  class =” sister " id =” li nk3 ”> T illie</a> ;
+and  they  lived  at  the  bottom  of  a well. </p>
+<p  class =” story ”>  ...  </p>
+"""
+
+soup = BeautifulSoup(html,'lxml')
+soup.prettify()
+print(soup.body)
--- 临时/test.py
+++ 临时/test.py
@@ -1,11 +1,4 @@
-
-
-
-l=[1,2,3]
-for i in l:
-    if i==2:
-        break
-    print(i)
-
-s1='"""'
-print(s1.startswith('"""'))
+if []==None:
+    print("[]")
+else:
+    print("none")
